3cf837a1e1f33045ac5ab7dc9cf45a9511627be0
Thomas Floyd <tefloyd1215@gmail.com>
2019-01-15 16:31:06 -0600

    -Created Lights Subsystem

---
+++ src/main/java/frc/robot/subsystems/Lights.java
@@ -0,0 +1,14 @@
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+
+public class Lights extends Subsystem {
+
+
+  @Override
+  public void initDefaultCommand() {
+
+  }
+}
