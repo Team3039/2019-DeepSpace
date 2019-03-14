ef300f3ee46387d5e43d0d77fbf3ebe640bb6556
Thomas Floyd <tefloyd1215@gmail.com>
2019-03-14 17:19:19 -0500

    -Swap back to Brushed Motors and TalonSRXs in case of NEO failure

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -1,10 +1,11 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMax.IdleMode;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import frc.robot.Constants;
 import frc.robot.Robot;
@@ -14,13 +15,18 @@ import frc.util.PS4Gamepad;
 
 public class Drivetrain extends Subsystem {
     //Drive Motors
-    public CANSparkMax frontleftMotor = new CANSparkMax(RobotMap.frontleftMotor, MotorType.kBrushless); 
-    public CANSparkMax frontrightMotor = new CANSparkMax(RobotMap.frontrightMotor, MotorType.kBrushless);
-    public CANSparkMax rearleftMotor = new CANSparkMax(RobotMap.rearleftMotor, MotorType.kBrushless);
-    public CANSparkMax rearrightMotor = new CANSparkMax(RobotMap.rearrightMotor, MotorType.kBrushless);
+    public TalonSRX frontleftMotor = new TalonSRX(RobotMap.frontleftMotor); 
+    public TalonSRX frontrightMotor = new TalonSRX(RobotMap.frontrightMotor);
+    public TalonSRX rearleftMotor = new TalonSRX(RobotMap.rearleftMotor);
+    public TalonSRX rearrightMotor = new TalonSRX(RobotMap.rearrightMotor);
+
+    public Solenoid cameraPivot = new Solenoid(RobotMap.cameraPivot);
 
     //Checks if Robot is being driven in Reverse
     public boolean isReversed = false;
+
+    //Checks if speed needs to be further limited
+    public boolean isSlow = false;
 
   public void joystickControl(PS4Gamepad gp) {
     //Tele-Op Driving
@@ -28,22 +34,24 @@ public class Drivetrain extends Subsystem {
     double y = gp.getLeftYAxis()*-Constants.y;
     double rot = gp.getRightXAxis()*Constants.rot;
 
-    //Calculated Outputs (Assuming SPARK MAXs limit Output Voltage according to Bus Voltage)
-    double leftOutput = y + rot;
-    double rightOutput = rot - y;
+    frontleftMotor.setNeutralMode(NeutralMode.Brake);
+    frontrightMotor.setNeutralMode(NeutralMode.Brake);
+    rearrightMotor.setNeutralMode(NeutralMode.Brake);
+    rearleftMotor.setNeutralMode(NeutralMode.Brake);
 
-    //Set Motor's Neutral/Idle Mode to Brake
-    frontleftMotor.setIdleMode(IdleMode.kBrake);
-    frontrightMotor.setIdleMode(IdleMode.kBrake);
-    rearrightMotor.setIdleMode(IdleMode.kBrake);
-    rearleftMotor.setIdleMode(IdleMode.kBrake);
-
-    //Assigns Each Motor's Power
-    frontleftMotor.set(leftOutput);
-    frontrightMotor.set(rightOutput);
-    rearleftMotor.follow(frontleftMotor);
-    rearrightMotor.follow(frontrightMotor);
+    frontleftMotor.set(ControlMode.PercentOutput, (y+rot)/2);
+    frontrightMotor.set(ControlMode.PercentOutput, (rot-y)/2);
+    rearleftMotor.set(ControlMode.PercentOutput, (y+rot)/2);
+    rearrightMotor.set(ControlMode.PercentOutput, (rot-y)/2);
+    
+    if(y < 0) {
+      isReversed = true;
+    }
+    else {
+      isReversed = false;
+    }
   }
+
 
   public void setTrackingMode() {
     NetworkTableInstance.getDefault().getTable("limelight").getEntry("ledMode").setNumber(3); //Turns LED on
@@ -69,18 +77,22 @@ public class Drivetrain extends Subsystem {
       rot = gp.getRightXAxis(); //If LL2 does not see a target, spin in place
     }
 
-    frontleftMotor.set(y+rot);
-    frontrightMotor.set(rot-y);
-    rearleftMotor.set(y+rot);
-    rearrightMotor.set(rot-y);
+    frontleftMotor.set(ControlMode.PercentOutput, (y+rot)/2);
+    frontrightMotor.set(ControlMode.PercentOutput, (rot-y)/2);
+    rearleftMotor.set(ControlMode.PercentOutput, (y+rot)/2);
+    rearrightMotor.set(ControlMode.PercentOutput, (rot-y)/2);
   }
 
   public void stop() {
     //Stops all drive
-    frontleftMotor.set(0);
-    frontrightMotor.set(0);
-    rearleftMotor.set(0);
-    rearrightMotor.set(0);   
+    frontleftMotor.set(ControlMode.PercentOutput, 0);
+    frontrightMotor.set(ControlMode.PercentOutput, 0);
+    rearleftMotor.set(ControlMode.PercentOutput, 0);
+    rearrightMotor.set(ControlMode.PercentOutput, 0);   
+  }
+
+  public void cameraPivot(boolean state) {
+    cameraPivot.set(!state);
   }
   
   @Override
